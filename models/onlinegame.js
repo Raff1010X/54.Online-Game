cc0e04e59d032695188f4ac0b770eb2678814dfa
Rafał Kowalczyk <kowalczykr@trendglass.pl>
2023-03-29 14:58:09 +0200

    add: onlinegame model sort

--- models/onlinegame.js
+++ models/onlinegame.js
@@ -1,4 +1,9 @@
 exports.calculate = (data) => {
+    let { groupCount, clans } = data;
+    clans.sort((a, b) => a.numberOfPlayers - b.numberOfPlayers);
+    clans.sort((a, b) => b.points - a.points);
+
+
     
-    return "response form calculate";
+    return clans;
 };
