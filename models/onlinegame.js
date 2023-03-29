337758d21d6fb6f638bb0ef2c9c33d349daa69df
Rafał Kowalczyk <raff1410X@gmail.com>
2023-03-29 19:38:17 +0200

    update: onlinegame model

--- models/onlinegame.js
+++ models/onlinegame.js
@@ -1,9 +1,27 @@
 exports.calculate = (data) => {
     let { groupCount, clans } = data;
-    clans.sort((a, b) => a.numberOfPlayers - b.numberOfPlayers);
-    clans.sort((a, b) => b.points - a.points);
 
+    // Sort clans by number of players and then by points
+    clans
+        .sort((a, b) => a.numberOfPlayers - b.numberOfPlayers)
+        .sort((a, b) => b.points - a.points);
 
+    // Separate clans into head and tail based on number of players
+    let head = [],
+        tail = [];
+    clans.forEach((el) =>
+        el.numberOfPlayers <= groupCount ? head.push(el) : tail.push(el)
+    );
+
+    //TODO // Iterate through head and group clans based on number of players
+    let result = [],
+        temp = [];
     
-    return clans;
+    // Add any remaining clans in temp to the result array
+    if (temp.length > 0) {
+        result.push(temp);
+    }
+
+    // Merge the grouped head clans with the tail clans and return the result
+    return result.concat(tail);
 };
