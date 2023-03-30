7048d7e4d10c5a9656371526c76d42866d3a07e1
Rafał Kowalczyk <raff1410X@gmail.com>
2023-03-30 18:40:11 +0200

    update: onlinegame model

--- models/onlinegame.js
+++ models/onlinegame.js
@@ -1,21 +1,32 @@
 exports.calculate = (data) => {
-    const { groupCount } = data;
-    const clansCopy = data.clans.slice().sort((a, b) => {
-        return a.points === b.points ? a.numberOfPlayers - b.numberOfPlayers : b.points - a.points;
+    let { groupCount, clans } = data;
+
+    clans = [...clans].sort((a, b) => {
+        return a.points === b.points
+            ? a.numberOfPlayers - b.numberOfPlayers
+            : b.points - a.points;
     });
-    console.log(clansCopy)
-    let result = [], group = [], skip = [], sum = 0;
-    do {
-        sum += clansCopy[0].numberOfPlayers;
-        sum <= groupCount ? group.push(clansCopy[0]) : (skip.push(clansCopy[0]), (sum -= clansCopy[0].numberOfPlayers));
-        clansCopy.shift();
-        if (clansCopy.length === 0 || sum === groupCount) {
+
+    let result = [],
+        group = [],
+        skip = [],
+        clan = [],
+        sum = 0;
+
+    while (clans.length > 0) {
+        clan = clans.shift();
+
+        sum + clan.numberOfPlayers <= groupCount
+            ? (group.push(clan), (sum += clan.numberOfPlayers))
+            : skip.push(clan);
+
+        if (clans.length === 0 || sum === groupCount) {
+            clans.unshift(...skip);
             result.push(group);
             group = [];
-            sum = 0;
-            clansCopy.unshift(...skip);
             skip = [];
+            sum = 0;
         }
-    } while (clansCopy.length > 0);
+    }
     return result;
 };
