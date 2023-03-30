49e02ed08df5e9aa0f1303ef9b6fe23d7a9ee431
Rafał Kowalczyk <kowalczykr@trendglass.pl>
2023-03-30 13:14:58 +0200

    update: onlinegame model

--- controllers/onlinegame.js
+++ controllers/onlinegame.js
@@ -3,7 +3,6 @@ const example = require('../example/example_request.json')
 
 exports.calculate = (req, res, next) => {
     const data = req.body || example;
-    console.log(example)
     const response = onlinegame.calculate(data);
     res.status(200).send(response);
 };
--- models/onlinegame.js
+++ models/onlinegame.js
@@ -1,13 +1,6 @@
-exports.calculate2 = (data) => {
-    return data;
-};
-
 exports.calculate = (data) => {
-    let { groupCount } = data;
-    let clansCopy = data.clans.slice();
-
-    // Sort clans by number of players and then by points
-    clansCopy.sort((a, b) => {
+    const { groupCount } = data;
+    const clansCopy = data.clans.slice().sort((a, b) => {
         if (a.points === b.points) {
             return a.numberOfPlayers - b.numberOfPlayers;
         }
@@ -15,86 +8,35 @@ exports.calculate = (data) => {
     });
 
     // Iterate through head and group clansCopy based on number of players
-    let result = [];
-    let group = [];
-    let skip = [];
-    let sum = 0;
-    let players = 0;
+    let result = [],
+        group = [],
+        skip = [],
+        sum = 0,
+        players = 0,
+        len = clansCopy.length;
 
-    for (let i = 0; i < clansCopy.length; i++) {
-        players = clansCopy[i].numberOfPlayers;
+    do {
+        players = clansCopy[0].numberOfPlayers;
         sum += players;
-
-        if (sum <= groupCount) {
-            group.push(clansCopy[i]);
-            if (sum === groupCount) {
-                result.push(group);
-                group = [];
-                sum = 0;
-                if (skip.length > 0) {
-                    clansCopy.splice(i + 1, 0, ...skip);
-                    skip = [];
-                }
-            }
-        }
-
-        if (sum > groupCount) {
-            skip.push(clansCopy[i]);
+        if (sum <= groupCount) group.push(clansCopy[0]);
+        else {
+            skip.push(clansCopy[0]);
             sum -= players;
         }
+        clansCopy.shift();
+        len -= 1;
 
-        if (i === clansCopy.length - 1) {
+        if (len === 0 || sum === groupCount) {
             result.push(group);
             group = [];
             sum = 0;
             if (skip.length > 0) {
-                clansCopy.splice(i + 1, 0, ...skip);
+                clansCopy.unshift(...skip);
+                len += skip.length;
                 skip = [];
             }
         }
-    }
+    } while (len > 0);
 
     return result;
 };
-
-// Iterate through head and group clansCopy based on number of players
-// let result = [],
-//     group = [],
-//     skip = [],
-//     sum = 0,
-//     players = 0;
-
-// do {
-//     players = clansCopy[0].numberOfPlayers;
-//     sum += players;
-
-//     if (sum <= groupCount) {
-//         group.push(clansCopy[0]);
-//         clansCopy.shift();
-//         if (sum === groupCount) {
-//             result.push(group);
-//             group = [];
-//             sum = 0;
-//             if (skip.length > 0) {
-//                 clansCopy.unshift(...skip);
-//                 skip = [];
-//             }
-//         }
-//     }
-
-//     if (sum > groupCount) {
-//         skip.push(clansCopy[0]);
-//         sum -= players;
-//         clansCopy.shift();
-//     }
-
-//     if (clansCopy.length === 0) {
-//         result.push(group);
-//         group = [];
-//         sum = 0;
-//         if (skip.length > 0) {
-//             clansCopy.unshift(...skip);
-//             skip = [];
-//         }
-//     }
-// } while (clansCopy.length > 0);
