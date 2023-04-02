932ac110c69a29d57e0631c763726835c582b87a
Rafał Kowalczyk <raff1410X@gmail.com>
2023-04-02 19:16:10 +0200

    update: simple test

--- models/onlinegame.test.js
+++ models/onlinegame.test.js
@@ -2,9 +2,9 @@ const { calculate } = require('./onlinegame');
 const data = require('../example/example_request.json')
 const expected = require('../example/example_response.json')
 
-describe('report', () => {
+describe('calculate', () => {
 
-  it('should return correct account information', () => {
+  it('should return correct data', () => {
     const result = calculate(data);
     expect(result).toEqual(expected);
   });
